dcecfc11d8e6fbeb9c4ea0c30b69ce7711d9dce9
mybarefootstory <akashkumarswarnkar7172@gmail.com>
2025-06-03 18:39:25 +0530

    Data Ingestion Completed

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from src.Text_Summarizer.logging import logger
+from src.Text_Summarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("Starting the main function")
+STAGE_NAME = "Data Ingestion stage"
 
-
+try:
+    logger.info(f"\n\nStage {STAGE_NAME} initiated >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.")
+    data_ingestion_pipeline=DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"\n\nStage {STAGE_NAME} completed successfully <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<.")
+except Exception as e:
+    logger.exception(e)
+    raise e
